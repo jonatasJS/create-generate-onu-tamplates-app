8c7bdf6d5a3e0f3949f8bf74be9bcffea27a8f41
jonatasJS <soaresjonatas398@gmail.com>
2022-06-03 19:17:54 -0300

    teste frame motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 
+import { motion } from "framer-motion";
+
 import GlobalStyle from "../styles/globalStyle";
 import theme from "../styles/theme/light";
 
@@ -9,7 +11,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <motion.div initial="pageInitial" animate="pageAnimate" variants={{
+        
+      }} exit="exit">
+        <Component {...pageProps} />
+      </motion.div>
     </ThemeProvider>
   );
 }
