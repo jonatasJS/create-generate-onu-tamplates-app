abae44000587ea3c5bbbb295d6fdc6f58bf50c71
jonatasJS <soaresjonatas398@gmail.com>
2022-06-14 19:13:21 -0300

    teste

---
+++ pages/api/olt/profiles/index.ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import data from './data.json';
+
+type Data = {
+  name: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  res.status(200).json(data)
+}
