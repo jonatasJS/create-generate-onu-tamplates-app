f9626cd66b8d262244bd1a5dd5c64e487fb04916
jonatasJS <soaresjonatas398@gmail.com>
2022-07-15 08:08:27 -0300

    fixxxxxxxx

--- components/ButtonBack/index.tsx
+++ components/ButtonBack/index.tsx
@@ -19,12 +19,15 @@ export default function ButtonBack({
   value,
   children,
 }: ButtonBackProps) {
+
+  console.log(styles);
+  console.log(styles.buttonBack);
   return (
     <motion.button
       onClick={onClick}
       whileHover={{ scale: 1.1, zIndex: 9999 }}
       whileTap={{ scale: 0.9 }}
-      className={`${styles.buttonBack} ${!isArrow ? styles.arrow : ""}`}
+      className={`${styles.goBackPage} ${!isArrow ? styles.arrow : ""}`}
       style={style}
     >
       {isArrow && <ArrowLeft width={20} height={20} />} {value || children}
