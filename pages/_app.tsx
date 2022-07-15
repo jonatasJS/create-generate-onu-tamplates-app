953eeaa9285914ca1f6c2417584643bd3e8d77e1
jonatasJS <soaresjonatas398@gmail.com>
2022-07-15 14:25:58 -0300

    sdfsadfasd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,8 @@ import { UserProvider, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { ThemeProvider } from "styled-components";
 import { motion, AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
-import { CursorProvider } from "react-cursor-custom";
+import CustomCursor from "custom-cursor-react";
+import "custom-cursor-react/dist/index.css";
 
 import { WhatsAppButton } from "../components/WhatsAppButton";
 import ButtonBack from "../components/ButtonBack";
@@ -40,89 +41,99 @@ function MyApp({ Component, pageProps, router }: AppProps) {
   }, [cursorX, cursorY]);
 
   return (
-    <CursorProvider>
-      <UserProvider>
-        <div
-          className="cursor"
-          style={{
-            // left: cursorX - 10,
-            // top: cursorY - 10,
-            zIndex: 9999999999999999999,
+    <UserProvider>
+      <CustomCursor
+        targets={["a", "span", "button", "input", "textarea"]}
+        customClass="custom-cursor"
+        dimensions={30}
+        fill="#FFF"
+        smoothness={{
+          movement: 0.2,
+          scale: 0.1,
+          opacity: 0.2,
+        }}
+        targetOpacity={0.5}
+      />
+      <div
+        className="cursor"
+        style={{
+          // left: cursorX - 10,
+          // top: cursorY - 10,
+          zIndex: 9999999999999999999,
+        }}
+      ></div>
+      {router.pathname !== "/" && (
+        <ButtonBack isArrow={true} onClick={back} value="VOLTAR" />
+      )}
+      <AnimatePresence exitBeforeEnter={true}>
+        <motion.div
+          key={router.pathname}
+          initial="pageInitial"
+          animate="pageAnimate"
+          exit="pageExit"
+          transition={{
+            type: "spring",
+            stiffness: 700,
+            damping: 30,
           }}
-        ></div>
-        {router.pathname !== "/" && (
-          <ButtonBack isArrow={true} onClick={back} value="VOLTAR" />
-        )}
-        <AnimatePresence exitBeforeEnter={true}>
-          <motion.div
-            key={router.pathname}
-            initial="pageInitial"
-            animate="pageAnimate"
-            exit="pageExit"
-            transition={{
-              type: "spring",
-              stiffness: 700,
-              damping: 30,
-            }}
-            variants={{
-              pageInitial: {
-                opacity: 0,
-                x: -100,
-                // scale: 0,
-              },
-              pageAnimate: {
-                opacity: 1,
-                x: 0,
-                // scale: 1,
-              },
-              pageExit: {
-                opacity: 0,
-                x: "15vw",
-                // scale: 0,
-              },
-            }}
-            style={{
-              width: "100vw",
-              height: "97vh",
-              display: "flex",
-              justifyContent: "center",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <ThemeProvider theme={theme}>
-              <Head>
-                <title>Gerador de template para configuração!</title>
-              </Head>
-
-              <GlobalStyle />
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </motion.div>
-        </AnimatePresence>
-
-        <WhatsAppButton />
-
-        <footer
-          style={{ top: "47vw", position: "fixed" }}
-          className={styles.footer}
+          variants={{
+            pageInitial: {
+              opacity: 0,
+              x: -100,
+              // scale: 0,
+            },
+            pageAnimate: {
+              opacity: 1,
+              x: 0,
+              // scale: 1,
+            },
+            pageExit: {
+              opacity: 0,
+              x: "15vw",
+              // scale: 0,
+            },
+          }}
+          style={{
+            width: "100vw",
+            height: "97vh",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          <Link href="https://api.whatsapp.com/send?phone=5563991321180&text=Ol%C3%A1%2C%20gostaria%20de%20verificar%20um%20or%C3%A7amento.">
-            <a target="_blank" rel="noopener noreferrer">
-              Powered by{" "}
-              <span className={styles.logo}>
-                <img
-                  src="/jonatas.svg"
-                  alt="Criador por Jônatas"
-                  width={72}
-                  height={16}
-                />
-              </span>
-            </a>
-          </Link>
-        </footer>
-      </UserProvider>
-    </CursorProvider>
+          <ThemeProvider theme={theme}>
+            <Head>
+              <title>Gerador de template para configuração!</title>
+            </Head>
+
+            <GlobalStyle />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </motion.div>
+      </AnimatePresence>
+
+      <WhatsAppButton />
+
+      <footer
+        style={{ top: "47vw", position: "fixed" }}
+        className={styles.footer}
+      >
+        <Link href="https://api.whatsapp.com/send?phone=5563991321180&text=Ol%C3%A1%2C%20gostaria%20de%20verificar%20um%20or%C3%A7amento.">
+          <a target="_blank" rel="noopener noreferrer">
+            Powered by{" "}
+            <span className={styles.logo}>
+              <img
+                src="/jonatas.svg"
+                alt="Criador por Jônatas"
+                width={72}
+                height={16}
+              />
+            </span>
+          </a>
+        </Link>
+      </footer>
+    </UserProvider>
   );
 }
 
