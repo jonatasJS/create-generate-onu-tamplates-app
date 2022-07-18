bccb61907e616925c3a6ca2126ba38bb52af685b
jonatasJS <soaresjonatas398@gmail.com>
2022-07-18 14:14:57 -0300

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,21 @@
+function Page404() {
+  return (
+    <div>
+      <h1
+        style={{
+          width: "100vw",
+          height: "97vh",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        404
+      </h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
+
+export default Page404;
