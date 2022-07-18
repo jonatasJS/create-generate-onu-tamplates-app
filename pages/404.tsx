94a6ea07f131b209defb06dd1e40ba376bcc3123
jonatasJS <soaresjonatas398@gmail.com>
2022-07-18 14:23:49 -0300

    add and fix page 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,19 +1,30 @@
+import { useRouter } from "next/router";
+import ButtonBack from "../components/ButtonBack";
+
 function Page404() {
+  const { back } = useRouter();
   return (
-    <div>
-      <h1
+    <div
+      style={{
+        width: "100vw",
+        height: "97vh",
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1>404</h1>
+      <p>Page not found</p>
+      <ButtonBack
+        isArrow={false}
+        onClick={back}
         style={{
-          width: "100vw",
-          height: "97vh",
-          display: "flex",
-          justifyContent: "center",
-          flexDirection: "column",
-          alignItems: "center",
+          position: "unset",
         }}
       >
-        404
-      </h1>
-      <p>Page not found</p>
+        HOME
+      </ButtonBack>
     </div>
   );
 }
