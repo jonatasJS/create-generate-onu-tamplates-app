3dd55521c4ee4281bf41fd07c2fc844cc5c115df
jonatasJS <soaresjonatas398@gmail.com>
2022-08-06 08:01:09 -0300

    ..............

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-css-tags */
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable @next/next/next-script-for-ga */
 /* eslint-disable @next/next/no-title-in-document-head */
