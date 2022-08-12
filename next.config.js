29250f7b219c2f04b07019d02a08dcdc66032b0e
jonatasjs <soaresjonatas398@gmail.com>
2022-08-12 19:00:24 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPlugins = require('next-compose-plugins')
+const withPlugins = require('next-compose-plugins');
+const withPreact = require('next-plugin-preact')
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
   analyzerMode: "static",
@@ -15,20 +16,15 @@ const nextConfig = {
     domains: ['random.imagecdn.app'],
   },
   reactStrictMode: true,
+	webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    // Important: return the modified config
+    return config
+  }
 };
 const withSVGR = require('next-svgr');
-const webpack = (config, { dev, isServer }) => {
-	// Replace React with Preact only in client production build
-	if (!dev && !isServer) {
-		Object.assign(config.resolve.alias, {
-			react: 'preact/compat',
-			'react-dom/test-utils': 'preact/test-utils',
-			'react-dom': 'preact/compat'
-		})
-	}
-
-	return config
-}
 
 module.exports = withPlugins([
   // [withPWA, { pwa: {
@@ -64,6 +60,6 @@ module.exports = withPlugins([
   // } }],
   withSVGR,
   withBundleAnalyzer,
-	webpack
+	withPreact,
 ], nextConfig);
 // module.exports = nextConfig;
