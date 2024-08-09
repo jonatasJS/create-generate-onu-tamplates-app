e06fb71c7ecba14d8e8c078e80fdd22ec8327335
sozinho <soaresjonatas398@gmail.com>
2024-08-09 19:06:56 +0000

    quem leu é gay

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,32 +18,36 @@ const Home: NextPage = () => {
 	}
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Gerador de template para configuração!</title>
-      </Head>
+		<div className={styles.container}>
+			<Head>
+				<title>Gerador de template para configuração!</title>
+			</Head>
 
-      <HomeTheme>
-        <main className={styles.main}>
-
-					<Typewriter options={{
-						strings: [
-							`<h1 class="${styles.title}">
+			<HomeTheme>
+				<main className={styles.main}>
+					<Typewriter
+						options={{
+							strings: [
+								`<h1 class="${styles.title}">
 								Gerador de template para <a style="background-color: transparent !important;border: none !important;" onclick="${handleClick}">configuração</a>!
 							</h1>`,
-							`<h1 class="${styles.title}">
+								`<h1 class="${styles.title}">
 								Qualquer bug ou problema: <a class="${styles.code}" target="_blank" href="https://bit.ly/3QCpUYr">Jônatas - NOC 1</a>
 							</h1>`,
-						],
-						autoStart: true,
-						delay: 51,
-						loop: true,
-						deleteSpeed: 0,
-						cursor: '|'
-					}} />
+								,
+								`<h1 class="${styles.title}">
+								Quem leu isso é gay
+							</h1>`,
+							],
+							autoStart: true,
+							delay: 51,
+							loop: true,
+							deleteSpeed: 0,
+							cursor: "|",
+						}}
+					/>
 
-
-          {/* <p className={styles.description}>
+					{/* <p className={styles.description}>
             Qualquer bug ou problema{": "}
             <span className={styles.code}>
               <Link href="https://api.whatsapp.com/send?phone=5563991321180&text=Ol%C3%A1%2C%20estou%20com%20problema%20no%20gerador%20de%20templates!">
@@ -51,57 +55,57 @@ const Home: NextPage = () => {
               </Link>
             </span>
           </p> */}
-          <div className={styles.grid}>
-            <Link href="/furukawa">
-              <motion.a
-                whileHover={{ scale: 1.1, zIndex: 9999 }}
-                whileTap={{ scale: 0.9 }}
-                className={styles.card}
-              >
-                <h2>Furukawa &rarr;</h2>
-                <p>Gera template para instalação de onu Furukawa</p>
-              </motion.a>
-            </Link>
+					<div className={styles.grid}>
+						<Link href="/furukawa">
+							<motion.a
+								whileHover={{ scale: 1.1, zIndex: 9999 }}
+								whileTap={{ scale: 0.9 }}
+								className={styles.card}
+							>
+								<h2>Furukawa &rarr;</h2>
+								<p>Gera template para instalação de onu Furukawa</p>
+							</motion.a>
+						</Link>
 
-            <Link href="/parks">
-              <motion.a
-                whileHover={{ scale: 1.1, zIndex: 9999 }}
-                whileTap={{ scale: 0.9 }}
-                style={{ opacity: 0.5 }}
-                className={styles.card}
-              >
-                <h2>Parks &rarr;</h2>
-                <p>Gera template para instalação de onu Parks.</p>
-              </motion.a>
-            </Link>
+						<Link href="/parks">
+							<motion.a
+								whileHover={{ scale: 1.1, zIndex: 9999 }}
+								whileTap={{ scale: 0.9 }}
+								style={{ opacity: 0.5 }}
+								className={styles.card}
+							>
+								<h2>Parks &rarr;</h2>
+								<p>Gera template para instalação de onu Parks.</p>
+							</motion.a>
+						</Link>
 
-            <Link href="/format">
-              <motion.a
-                whileHover={{ scale: 1.1, zIndex: 9999 }}
-                whileTap={{ scale: 0.9 }}
-                className={styles.card}
-              >
-                <h2>Formatar &rarr;</h2>
-                <p>Formatar contarto para{"\n"}colocar no nome da onu!</p>
-              </motion.a>
-            </Link>
+						<Link href="/format">
+							<motion.a
+								whileHover={{ scale: 1.1, zIndex: 9999 }}
+								whileTap={{ scale: 0.9 }}
+								className={styles.card}
+							>
+								<h2>Formatar &rarr;</h2>
+								<p>Formatar contarto para{"\n"}colocar no nome da onu!</p>
+							</motion.a>
+						</Link>
 
-            <Link href="/">
-              <motion.a
-                whileHover={{ scale: 1.1, zIndex: 9999 }}
-                whileTap={{ scale: 0.9 }}
-                style={{ opacity: 0.5 }}
-                className={styles.card}
-              >
-                <h2>FUTURO &rarr;</h2>
-                <p>Implemetação futura!{"\n"}Implemetação futura!</p>
-              </motion.a>
-            </Link>
-          </div>
-        </main>
-      </HomeTheme>
-    </div>
-  );
+						<Link href="/">
+							<motion.a
+								whileHover={{ scale: 1.1, zIndex: 9999 }}
+								whileTap={{ scale: 0.9 }}
+								style={{ opacity: 0.5 }}
+								className={styles.card}
+							>
+								<h2>FUTURO &rarr;</h2>
+								<p>Implemetação futura!{"\n"}Implemetação futura!</p>
+							</motion.a>
+						</Link>
+					</div>
+				</main>
+			</HomeTheme>
+		</div>
+	);
 };
 
 export default Home;
@@ -128,3 +132,4 @@ export default Home;
 //     props: {},
 //   };
 // };
+
